8ab6224aeec31804dc514d843ddc11a98398d327
kabinSherma <kabinshermalimbu@gmail.com>
2025-05-05 19:00:48 +0545

    javaScript

---
+++ script.js
@@ -0,0 +1,9 @@
+        let navLink=document.querySelector(".link");
+        function showMenu(){
+            navLink.style.right='0';
+        }
+        function hideMenu()
+        {
+            navLink.style.right='-200px';
+        }
+        
