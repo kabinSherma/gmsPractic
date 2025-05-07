25e4a8da0d535a85bef8286f71b7ffb496de1be7
kabinSherma <kabinshermalimbu@gmail.com>
2025-05-07 12:16:17 +0545

    nav bar none

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
         let navLink=document.querySelector(".link");
         function showMenu(){
             navLink.style.right='0';
+            navLink.style.display="block";
         }
         function hideMenu()
         {
             navLink.style.right='-200px';
+            navLink.style.display="none";
         }
         
